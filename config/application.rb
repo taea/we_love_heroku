0e5fa95b7e1306b51eb7e6aa6e0816942b049aec
koshikawa <koshikawa@ppworks.jp>
2012-02-21 21:32:52 +0900

    Heroku requires this to be false
    see
    http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,8 @@ module Gallery
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
