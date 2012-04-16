0b4d195cb8c6d39321f1e04ef44d3a94ee805bb6
koshikawa <koshikawa@ppworks.jp>
2012-04-17 00:52:40 +0900

    Revert "Add rooms scaffold"
    This reverts commit 13288480bff841f4633f10a0a23d859d443bd8cc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Gallery::Application.routes.draw do
-  resources :rooms
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:sessions]
   devise_scope :user do
     match '/users/sign_out(.:format)', {:action=>"destroy", :controller=>"devise/sessions", :via => :delete, :as => :destroy_user_session }
--- spec/factories/rooms.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :room do
-    user_id 1
-    name "MyString"
-    description "MyText"
-  end
-end
