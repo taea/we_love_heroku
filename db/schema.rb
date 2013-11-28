eff051063aff1c9cedec0dd4a76c1f29e2d16ca1
koshikawa <koshikawa@ppworks.jp>
2013-11-28 15:16:52 +0900

    Add old DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20120701153705) do
+
+  create_table "providers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "providers", ["name"], name: "idx_name_on_providers", unique: true, using: :btree
+
+  create_table "providers_users", force: true do |t|
+    t.integer  "provider_id",   null: false
+    t.integer  "user_id",       null: false
+    t.string   "user_key",      null: false
+    t.string   "access_token",  null: false
+    t.string   "refresh_token"
+    t.string   "secret"
+    t.string   "name",          null: false
+    t.string   "email"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "providers_users", ["provider_id", "user_key"], name: "idx_provider_id_user_key_on_providers_users", unique: true, using: :btree
+  add_index "providers_users", ["user_id"], name: "idx_user_id_on_providers_users", using: :btree
+
+  create_table "sites", force: true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.text     "description"
+    t.string   "creator"
+    t.string   "hash_tag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "repository_url"
+    t.integer  "user_id"
+    t.boolean  "scheduled_access"
+    t.boolean  "please_design"
+  end
+
+  add_index "sites", ["user_id"], name: "idx_user_id_on_sites", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name",                             null: false
+    t.string   "image",                            null: false
+    t.integer  "default_provider_id", default: 1,  null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "idx_email_on_users", unique: true, using: :btree
 
 end
